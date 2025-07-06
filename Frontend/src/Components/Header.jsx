ef6b8a176a1829ecf95c9add74140655369b76c7
hadeer <109919062+HadeerMou@users.noreply.github.com>
2025-07-06 15:22:46 +0300

    dashboard test button

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -5,7 +5,7 @@ import { LuMenu } from "react-icons/lu";
 import { Link, useNavigate } from "react-router-dom";
 import { IoIosContact } from "react-icons/io";
 import { FaUserCircle } from "react-icons/fa";
-import LanguageDropdown from "@/components/LanguageDropdown";
+import LanguageDropdown from "@/Components/LanguageDropdown";
 
 function Header({
   toggleMenuVisibility,
