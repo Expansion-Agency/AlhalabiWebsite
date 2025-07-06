2103a21ca39536fa5685e50314f6b7cfd5abad0d
hadeer <109919062+HadeerMou@users.noreply.github.com>
2025-07-06 15:47:38 +0300

    @ deletion

--- Frontend/src/Dashboard/Components/productsChart.jsx
+++ Frontend/src/Dashboard/Components/productsChart.jsx
@@ -11,7 +11,7 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "../Components/ui/card";
+} from "../../Components/ui/card";
 
 import { allProducts, categories } from "../../productsData";
 
