b2f64bb15914e7d861324de7c6652cedfa8b4546
Hajar-Medhat <hagarmedhat2001@gmail.cpm>
2025-09-16 11:09:14 +0000

    redirect

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,7 +12,7 @@ import Resetpass from "./Pages/Resetpass";
 import Forgotpass from "./Pages/Forgotpass";
 import Profile from "./Pages/Profile";
 import OtpPage from "./Pages/OtpPage";
-import GoogleAuthCallback from "./Pages/GoogleAuthCallback";
+// import GoogleAuthCallback from "./Pages/GoogleAuthCallback";
 import GoogleRedirectHandler from "./Pages/GoogleRedirectHandler";
 import Layout from "./Layouts/Layout";
 import DashboardLayout from "./Layouts/DashboardLayout";
