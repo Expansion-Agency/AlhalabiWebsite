a8b456d0dbd54c6e77037fce6eae1529ffc106e8
hadeer <109919062+HadeerMou@users.noreply.github.com>
2025-09-18 16:07:35 +0300

    updates

--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -5,25 +5,20 @@ function About() {
   const { t } = useTranslation();
   return (
     <>
-      <img className="w-full h-70 lg:h-130" src="/assets/industrial.jpg" alt="" />
+      <img
+        className="w-full h-70 lg:h-130"
+        src="/assets/industrial.jpg"
+        alt=""
+      />
       <div className="flex flex-col lg:my-10 mx-5 lg:mx-20 text-amber-950">
-        <div className="flex flex-col mt-10 lg:w-3/4 shadow-md p-5 rounded-md">
-          <h2 className="font-bold mb-5 text-2xl lg:text-3xl">{t("about")}</h2>
-          <h2 className="font-bold mb-2">{t("aboutqoute")}</h2>
-          <p className="text-amber-950/80">{t("aboutcont")}</p>
-        </div>
+        <h2 className="mt-10 font-bold mb-5 text-2xl lg:text-3xl">{t("about")}</h2>
         <div className="flex flex-col mt-10 lg:w-3/4 shadow-md p-5 rounded-md">
           <h2 className="font-bold mb-5 text-2xl lg:text-3xl">{t("whowe")}</h2>
           <p className="text-amber-950/80">{t("whowecont")}</p>
         </div>
         <div className="flex flex-col mt-10 lg:w-3/4 shadow-md p-5 rounded-md">
           <h2 className="font-bold mb-5 text-2xl lg:text-3xl">{t("offer")}</h2>
-          <p className="text-amber-950/80">
-            - {t("industrial")}
-            <br /> -{t("gspare")}
-            <br /> - {t("customsol")}
-            <br />- {t("techsup")} <br /> - {t("fastdel")} <br /> {t("weqoute")}
-          </p>
+          <p className="text-amber-950/80">{t("offercont")}</p>
         </div>
         <div className="flex flex-col mt-10 lg:w-3/4 shadow-md p-5 rounded-md">
           <h2 className="font-bold mb-5 text-2xl lg:text-3xl">
